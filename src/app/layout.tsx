775d7f768073edc1f96509c18e8412a5429ae4ac
youkwhd <lolywk@tutanota.com>
2023-11-24 21:19:41 +0700

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+import './global.css'
+
+export const metadata: Metadata = {
+    title: 'React Select Interview',
+    description: 'React select interview solution',
+}
+
+export default ({ children }: { children: React.ReactNode }) => {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <main>
+            <p>hello</p>
+        </main>
+    )
+}
