223c44f8754e5fb4bf9f3f908ccb9efc8e6f5104
youkwhd <lolywk@tutanota.com>
2023-11-24 21:46:19 +0700

    feat: add, render countries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
+"use client";
+
+type Country = {
+    id: number,
+    name: string,
+};
+
+const countries: Country[] = [
+    {
+        id: 1,
+        name: "India",
+    },
+    {
+        id: 2,
+        name: "USA",
+    },
+    {
+        id: 3,
+        name: "France",
+    },
+];
+
 export default () => {
     return (
         <main>
-            <p>hello</p>
+            <input 
+                type="checkbox"
+                name="check-all"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    console.log(e);
+                }} />
+            <label htmlFor="check-all">Select All</label>
+            <ul>
+                {countries.map((country: Country) => {
+                    return (
+                        <li key={`${country.id}`}>
+                            <input 
+                                type="checkbox"
+                                name={`country-${country.id}`}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    console.log(e.target.checked);
+                                }} />
+                            <label htmlFor={`country-${country.id}`}>{country.name}</label>
+                        </li>
+                    );
+                })}
+            </ul>
         </main>
     )
 }
